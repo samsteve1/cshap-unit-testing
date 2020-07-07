cacf7c45aa615922870e9579cf82507b148cc9c3
Steve Idowu <idowusteve@ymail.com>
2020-07-07 09:33:01 +0100

    Project setup

---
+++ PrimeService.UnitTests/ReservationTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace PrimeService.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            
+        }
+    }
+}
