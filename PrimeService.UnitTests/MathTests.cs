a3c1a96e022367087f14388c40f5e3129bc7abaa
Steve Idowu <idowusteve@ymail.com>
2020-07-07 11:14:28 +0100

    Writing parameterized tests

--- PrimeService.UnitTests/MathTests.cs
+++ PrimeService.UnitTests/MathTests.cs
@@ -19,22 +19,13 @@ namespace PrimeService.UnitTests
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
-        public void Max_FirstArgumentisGreate_ReturnTheFirstArgument()
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
-            var result = _math.Max(2, 1);
-            Assert.That(result, Is.EqualTo(2));
-        }
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondargument()
-        {
-            var result = _math.Max(1, 2);
-            Assert.That(result, Is.EqualTo(2));
-        }
-        [Test]
-        public void Max_ArgumentsAreTheSame_ReturnTheSameArgument()
-        {
-            var result = _math.Max(1, 1);
-            Assert.That(result, Is.EqualTo(1));
+            var result = _math.Max(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
