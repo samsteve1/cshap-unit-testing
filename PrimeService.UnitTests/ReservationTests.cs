8df2596e7692ae307cc1b8c4803db21e5e35e316
Steve Idowu <idowusteve@ymail.com>
2020-07-07 10:03:50 +0100

    Testing - reservation class

--- PrimeService.UnitTests/ReservationTests.cs
+++ PrimeService.UnitTests/ReservationTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TestNinja.Fundamentals;
 
 namespace PrimeService.UnitTests
 {
@@ -6,9 +7,32 @@ namespace PrimeService.UnitTests
     public class ReservationTests
     {
         [Test]
-        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() // MethodName_Scenario_expectedBehaviour
         {
-            
+            // Arrange
+            var revervation = new Reservation();
+            // Act
+            var result = revervation.CanBeCancelledBy(new User{IsAdmin = true});
+            // Assert
+            Assert.IsTrue(result);
         }
+        [Test]
+        public void CanBeCancelledBy_UserIsOwner_ReturnsTrue()
+        {
+            var revervation = new Reservation();
+            var user = new User(){IsAdmin = false};
+            revervation.MadeBy = user;
+
+            var result = revervation.CanBeCancelledBy(user);
+            Assert.IsTrue(result);
+        }
+        [Test]
+        public void CanBeCancelledBy_UserIsNotAdminNorOwner_ReturnsFalse()
+        {
+            var revervation = new Reservation();
+            var result = revervation.CanBeCancelledBy(new User());
+            Assert.IsFalse(result);
+        }
+        
     }
 }
