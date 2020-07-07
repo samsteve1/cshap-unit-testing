18bdbf75d9a4642248210d4b383514704f0d39e6
Steve Idowu <idowusteve@ymail.com>
2020-07-07 11:07:03 +0100

    Using the Setup method

--- PrimeService.UnitTests/MathTests.cs
+++ PrimeService.UnitTests/MathTests.cs
@@ -5,33 +5,35 @@ namespace PrimeService.UnitTests
     [TestFixture]
     public class MathTests
     {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
         
         [Test]
         public void Add_WhenCalled_ReturnsSumOfArguments()
         {
-            var math = new Math();
-            var result =math.Add(1, 2);
+            var result = _math.Add(1, 2);
             Assert.That(result, Is.EqualTo(3));
         }
         [Test]
         public void Max_FirstArgumentisGreate_ReturnTheFirstArgument()
         {
-            var math = new Math();
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnTheSecondargument()
         {
-            var math = new Math();
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
             Assert.That(result, Is.EqualTo(2));
         }
         [Test]
         public void Max_ArgumentsAreTheSame_ReturnTheSameArgument()
         {
-            var math = new Math();
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
             Assert.That(result, Is.EqualTo(1));
         }
     }
